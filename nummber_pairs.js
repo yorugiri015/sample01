0267e68a116a361ad6bb14e3fb1bf40eaabf41f6
unknown <kazumi@4101105-PC.morijyobi.intra>
2012-04-03 11:28:14 +0900

    ああああ

---
+++ nummber_pairs.js
@@ -0,0 +1,13 @@
+var n=0;
+var nummber_of_pairs=1;
+var before_nummber_of_pairs=1;
+
+while(month<=12){
+	nummber_of_pairs
+	=nummber_of_pairs
+	+before_nummber_of_pairs;
+
+	before_nummber_of_pairs=
+	month=month+1;
+	console.log(123);
+}
